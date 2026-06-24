9343889466390f6661a8dc1c0bc0cc8b89fc4d4d
Konstantin Mikheev <mik.kon6610@gmail.com>
2026-06-24 14:12:16 +0200

    fix

--- src/style.css.ts
+++ src/style.css.ts
@@ -1,27 +1,28 @@
-import { globalStyle, style } from '@vanilla-extract/css';
+import { style } from '@vanilla-extract/css';
 
 const bottomBtn = style({
   position: 'fixed',
   zIndex: 2,
   width: '100%',
-  padding: '12px',
+  padding: '12px 20px',
   bottom: 0,
 });
 
 const container = style({
   display: 'flex',
-  padding: '1rem',
   flexDirection: 'column',
   gap: '1rem',
 });
 
-const box = style({
+const boxHero = style({
   display: 'flex',
-  padding: '1rem',
+  padding: '1rem 20px 20px',
   flexDirection: 'column',
   gap: '1rem',
-  borderRadius: '1rem',
-  backgroundColor: '#F3F4F5',
+  borderRadius: '0 0 2rem 2rem',
+  backgroundColor: '#FFFFFF',
+  textAlign: 'center',
+  overflow: 'hidden',
 });
 
 const row = style({
@@ -30,16 +31,102 @@ const row = style({
   justifyContent: 'space-between',
 });
 
-const switchItem = style({});
+const slidePhone = style({
+  width: '280px',
+  borderRadius: '2rem',
+  backgroundColor: '#E6E7E9',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: '1rem',
+  padding: '0 1rem 1rem',
+});
 
-globalStyle(`${switchItem} > span > span:first-child`, {
-  fontWeight: 500,
+const boxBP = style({
+  display: 'flex',
+  padding: '20px',
+  flexDirection: 'column',
+  gap: '1rem',
+  borderRadius: '2rem',
+  backgroundColor: '#FFFFFF',
+});
+
+const tag = style({
+  borderRadius: '9999px',
+  padding: '2px 8px',
+  backgroundColor: '#F2F3F5',
+});
+
+const slideAva = style({
+  width: '160px',
+  height: '210px',
+  borderRadius: '24px',
+  backgroundColor: '#F2F3F5',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1rem',
+  padding: '1rem',
+});
+
+const bannerCell = style({
+  border: '1px solid #F2F3F5',
+  borderRadius: '24px',
+  padding: '1rem',
+});
+
+const slideAch = style({
+  width: '160px',
+  height: '210px',
+  borderRadius: '24px',
+  backgroundColor: '#F2F3F5',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1rem',
+  padding: '1rem',
+  position: 'relative',
+  overflow: 'hidden',
+});
+
+const boxAccess = style({
+  display: 'flex',
+  padding: '20px',
+  flexDirection: 'column',
+  gap: '1rem',
+  borderRadius: '2rem 2rem 0 0',
+  backgroundColor: '#FFFFFF',
+});
+
+const list = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '0.5rem',
+});
+
+const listCell = style({
+  borderRadius: '24px',
+  backgroundColor: '#F2F3F5',
+  padding: '8px 1rem 8px 8px',
+});
+
+const listCellSecondary = style({
+  borderRadius: '24px',
+  padding: '8px 1rem 8px 8px',
+  border: '1px dashed #E6E7E9',
 });
 
 export const appSt = {
   bottomBtn,
   container,
-  box,
+  boxHero,
   row,
-  switchItem,
+  slidePhone,
+  boxBP,
+  tag,
+  slideAva,
+  bannerCell,
+  slideAch,
+  boxAccess,
+  list,
+  listCell,
+  listCellSecondary,
 };
